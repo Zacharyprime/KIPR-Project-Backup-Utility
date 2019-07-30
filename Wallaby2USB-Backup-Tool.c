dceca86b52b841c432c1f2130fad8e90b2b87ec0
Zachary Sasser <ztsasser@gmail.com>
2019-07-30 14:38:37 -0500

    Update Wallaby2USB-Backup-Tool.c

--- Wallaby2USB-Backup-Tool.c
+++ Wallaby2USB-Backup-Tool.c
@@ -9,7 +9,7 @@ int main()
     system("cp -r ~/Documents/KISS/* /mnt");
     
     printf("Removing unwanted files... \n");
-    system("cd /mnt && rm *.json && rm -r bin && rm -r data && rm -r lib && rm -r src && rm -r include && rm tester.c"); 
+    system("cd /mnt && rm *.json && rm -r bin && rm -r data && rm -r lib && rm -r src && rm -r include"); 
     
     printf("Ejecting Flash Drive... \n");
     system("eject /dev/sd??");
