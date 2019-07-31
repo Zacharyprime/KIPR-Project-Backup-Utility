213f411c874af3df988a70df410071877ea79da8
Zachary Sasser <ztsasser@gmail.com>
2019-07-31 11:48:40 -0500

    Update Wallaby2USB-Backup-Tool.c

--- Wallaby2USB-Backup-Tool.c
+++ Wallaby2USB-Backup-Tool.c
@@ -1,6 +1,10 @@
+/*
+KIPR Project Backup Utility
+by: Zachary Sasser
+*/    
 #include <kipr/botball.h>
 
-int main()
+int backup()
 {
     printf("Mounting USB Drive... \n");
     system("mount /dev/sd?? /mnt");
@@ -11,10 +15,59 @@ int main()
     printf("Removing unwanted files... \n");
     system("cd /mnt && rm *.json && rm -r bin && rm -r data && rm -r lib && rm -r src && rm -r include"); 
     
-    printf("Ejecting Flash Drive... \n");
-    system("eject /dev/sd??");
+    system("sudo unmount /dev/sd??");
     
-    printf("DONE! \n");
+    console_clear();
+    printf("\n \nAll projects backed up on flash drive! \n");
 
+    return 1;
+}
+
+int restore(){
+	printf("Mounting USB drive... \n");
+	system("mount /dev/sd?? /mnt");
+	
+	printf("Moving projects from flash drive to the Controller... \n");
+	system("cp -r /mnt/* ~/Documents/KISS");
+	
+    system("sudo unmount /dev/sd??");
+    
+    console_clear();
+	printf("\n \nAll programs restored with USB flash drive \n \n");
+    
+	return 1;
+}
+
+int clearDrive(){
+    printf("Clearing flash drive... \n");  
+    system("umount /dev/sd??");
+    system("sudo mkfs.vfat -F 32 /dev/sd??");
+    
+    printf("\nFlash drive is now cleared. \n \n");
+    return 1;
+}
+
+int main(){
+	printf("Welcome to the KIPR Project Backup Utility! \n \n");
+	printf("Press A to move your projects to a flash drive. \n");
+	printf("Press B to use the backup stored on your flash drive! \n");
+    printf("Press C to clear your flash drive \n");
+	
+	while(!a_button() && !b_button() && !c_button()){
+	//Wait for a button press
+	}
+	if(a_button()){
+		console_clear();
+        backup();
+	}
+	else if(b_button()){
+        console_clear();
+		restore();
+	}
+    else if(c_button()){
+        console_clear();
+		clearDrive();
+	}
+    msleep(2000);
     return 0;
 }
